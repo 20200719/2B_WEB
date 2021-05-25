33c1a6e87d06e296e15f6f88c6a4d6264185fcfc
20200719 <20200719@g.kit.ac.kr>
2021-05-25 17:18:45 +0900

    13주

--- app.py
+++ app.py
@@ -8,13 +8,54 @@ def index():
     '''
     각주인데 뭐넣지
     '''
-@app.route('/hell')
-def hell():
-    return render_template("main.html")
-
 @app.route('/hello')
 def hello():
+    return render_template("main.html")
+
+@app.route('/hell')
+def hell():
     return '테스트로 돌리는 2서버인데요'
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
+@app.route('/action_page', methods=['GET','post'])
+def action_page():
+    if request.method == 'GET':
+        return "데이터를 받아주는 페이지"
+    else:
+        #여기 POST로 들어오는 데이터를 받아보자
+        email = request.form['email']
+        pwd = request.form['pwd']
+        print("전달된값:", email, pwd)
+        return '회원가입 데이터(post)<br>{}입니다.'
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/login_page', methods=['GET','post'])
+def login_page():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        #여기 POST로 들어오는 데이터를 받아보자
+        email = request.form['email']
+        pwd = request.form['pwd']
+        print("전달된값:", email, pwd)
+        #만약에 이메일과 패스워드 같다면
+        if email == 'a@a.com' and pwd == '1234':
+        #로그인 성공
+            return '로그인 성공'
+        #아니면
+        else:
+        #아이디 패스워드 확인
+            return '아이디 패스워드 확인'
+        
+        return '로그인 데이터(post)'
+    
+
 
 @app.route('/naver')
 def naver():
